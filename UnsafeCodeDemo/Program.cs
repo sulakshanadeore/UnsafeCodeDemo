3ea357ec32c71020a4b374327cf084de8886aae0
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-08 11:40:36 +0530

    Add project files.

---
+++ UnsafeCodeDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnsafeCodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int p = 100;
+                int* ptr;
+                ptr = &p;
+                Console.WriteLine("I m in the unsafe code block ");
+                Console.WriteLine("The value of p= " + *ptr);
+            
+            }
+            Employee emp = new Employee();
+            emp.Empid = 1023;
+            Console.WriteLine(emp.Empid);
+
+
+            Console.ReadLine();
+
+            //normal code
+        }
+    }
+
+    class Employee
+    {
+        public int Empid { get; set; }
+    }
+}
